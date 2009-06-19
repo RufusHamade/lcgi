c4d1a1a4cba1edbd8c80d0f4bf1ac35c8ab60f18
Rufus Hamade <rufus@myfanwy.org.uk>
2009-06-19 11:31:35 +0100

    Remember last directory visited when opening cgi file.

--- content/overlay.js
+++ content/overlay.js
@@ -1,5 +1,7 @@
 // This object implements the details of the "Open local CGI" menu item
 var lcgi = {
+  startDirectory: null,
+
   onLoad: function() {
     // initialization code
     this.initialized = true;
@@ -12,11 +14,15 @@ var lcgi = {
     fp.init(window, this.strings.getString('description'), nsIFilePicker.modeOpen);
     fp.appendFilters(nsIFilePicker.filterAll | nsIFilePicker.filterApps);
 
-    var dd = Components.classes["@mozilla.org/file/directory_service;1"].getService(Components.interfaces.nsIProperties).get("Home", Components.interfaces.nsIFile);
-    fp.displayDirectory = dd;
+    if (!lcgi.startDirectory) {
+      lcgi.startDirectory = Components.classes["@mozilla.org/file/directory_service;1"].getService(Components.interfaces.nsIProperties).get("Home", Components.interfaces.nsIFile);
+    }
+
+    fp.displayDirectory = lcgi.startDirectory;
 
     var rv = fp.show();
     if (rv == nsIFilePicker.returnOK) {
+      lcgi.startDirectory = fp.file.parent;
       var uri = 'lcgi:' + fp.file.path;
       loadURI(uri, null, null, true);
     }
